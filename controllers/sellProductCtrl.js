7b37fbe946e42d85d12900a4bbff65ff82a989fa
Sarah Morris <sarah.morris9@gmail.com>
2018-04-20 11:54:19 -0500

    Current date is showing up when posting product

--- controllers/sellProductCtrl.js
+++ controllers/sellProductCtrl.js
@@ -8,8 +8,23 @@ module.exports.displaySellProduct = (req, res) => {
   res.render('sellProduct');
 };
 
+// Today's Date function
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+if(dd<10) {
+    dd = '0'+dd
+} 
+if(mm<10) {
+    mm = '0'+mm
+} 
+today = mm + '/' + dd + '/' + yyyy;
+
 module.exports.sellProduct = (req, res, next) => {
   const { Product } = req.app.get('models');
+  req.body.user_id = 1;
+  req.body.listing_date = today;
   Product.create(req.body).then(data => {
     console.log("new data", data);
     res.json(data)
