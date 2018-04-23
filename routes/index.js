46f65841c524c8f3c9d096787056a71ff6e1f8db
Sarah Morris <sarah.morris9@gmail.com>
2018-04-23 14:14:50 -0500

    Fixed more merge conflicts and have it going to the correct route now with prod data

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,8 @@ router.get('/', (req, res, next) => {
 // pipe all other requests through the route modules
 router.use(require('./authRoute'));
 router.use(require('./productDetailRoute'));
-<<<<<<< HEAD
 router.use(require('./sellProductRoute'));
-=======
 router.use(require('./productRoute'));
->>>>>>> master
 router.use(require('./paymentTypeRoute'));
 router.use(require('./profileRoute'));
 // router.use(require('./foo'));
